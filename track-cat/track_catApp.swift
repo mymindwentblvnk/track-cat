3c298eff66656ae33b5ebb03b4ea15efc7734931
Michael Eichenseer <michael.eichenseer@codecentric.de>
2022-01-14 11:15:51 +0100

    Initial Commit

---
+++ track-cat/track_catApp.swift
@@ -0,0 +1,17 @@
+//
+//  track_catApp.swift
+//  track-cat
+//
+//  Created by Michael Eichenseer on 14.01.22.
+//
+
+import SwiftUI
+
+@main
+struct track_catApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
